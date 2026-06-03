e3d1d895f122c1ddfcf95b82fd4e230599789faf
Corrado Pacelli <corrado.pacel@gmail.com>
2026-06-03 17:35:02 +0200

    I don't know exactly how, but in less then 1 day I managed to have a sloution working for part 2, I hope none will ever read this code...

--- 2025/day7/day7.cpp
+++ 2025/day7/day7.cpp
@@ -1,12 +1,84 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <set>
+#include <map>
 
 using namespace std;
 
 //1630 one star :)
+//47857642990160 SECOND STAR OLE!!!!
 
+int main () {
+
+    ifstream file("input.txt");
+
+    if (!file) {
+        cout << "Unable to open file";
+        return 1;
+    } 
+
+    string line{};
+    getline(file, line);
+
+    map<size_t, long long unsigned int> beams_positions_and_relative_timelines;
+    beams_positions_and_relative_timelines.insert(make_pair(line.find('S'), 1));
+
+    while (getline(file, line)) {
+        size_t position_of_splitter = line.find('^');
+        map<size_t, long long unsigned int> beams_positions_and_relative_timelines_to_be_added;
+        while (!(position_of_splitter == std::string::npos)){
+            auto it = beams_positions_and_relative_timelines.begin();
+            while (it != beams_positions_and_relative_timelines.end()) {
+                if ((*it).first == position_of_splitter){
+                    
+                    if (beams_positions_and_relative_timelines_to_be_added.empty()) {
+                        beams_positions_and_relative_timelines_to_be_added.insert( make_pair((*it).first - 1, (*it).second) );
+                        beams_positions_and_relative_timelines_to_be_added.insert( make_pair((*it).first + 1, (*it).second) );
+                    } else {
+                        auto b1 = beams_positions_and_relative_timelines_to_be_added.find( (*it).first - 1 );
+                        if ( b1 == beams_positions_and_relative_timelines_to_be_added.end()){
+                            beams_positions_and_relative_timelines_to_be_added.insert( make_pair((*it).first - 1, (*it).second) );
+                        } else {
+                            b1->second += (*it).second;
+                        }
+                        auto b2 = beams_positions_and_relative_timelines_to_be_added.find( (*it).first + 1 );
+                        if ( b2 == beams_positions_and_relative_timelines_to_be_added.end()){
+                            beams_positions_and_relative_timelines_to_be_added.insert( make_pair((*it).first + 1, (*it).second) );
+                        } else {
+                            b2->second += (*it).second;
+                        }
+                    }
+
+                    it = beams_positions_and_relative_timelines.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+            position_of_splitter = line.find('^', position_of_splitter + 1);
+        }
+        for (auto& beam : beams_positions_and_relative_timelines_to_be_added){
+            auto it = beams_positions_and_relative_timelines.find(beam.first);
+            if ( it == beams_positions_and_relative_timelines.end()){
+                beams_positions_and_relative_timelines.insert(beam);
+            } else {
+                it->second += beam.second;
+            }
+        }
+
+        long long unsigned int partial_timelines{0};
+        for (auto& beam : beams_positions_and_relative_timelines){
+            partial_timelines += beam.second;
+        }
+        cout << "Partial number of timelines: " << partial_timelines << endl;
+    }
+    long long unsigned int timelines{0};
+    for (auto& beam : beams_positions_and_relative_timelines){
+        timelines += beam.second;
+    }
+    cout << "Total number of timelines: " << timelines << endl;
+}
+
+/* part 1
 int main () {
 
     ifstream file("input.txt");
@@ -46,3 +118,4 @@ int main () {
     }
     cout << "Total number of beams: " << number_of_splits << endl;
 }
+*/
